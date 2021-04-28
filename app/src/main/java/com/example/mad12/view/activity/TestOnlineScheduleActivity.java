e65f3ba72da659cbb64fe3311984c5d6c956224d
VIET HOANG <viethoangat01@gmail.com>
2021-04-28 15:30:26 +0700

    Feature: Test Online
    Mô tả:
    - Custom 2 màn hình chọn đề thi và môn học online
    
    Signed-off-by: VIET HOANG <viethoangat01@gmail.com>

---
+++ app/src/main/java/com/example/mad12/view/activity/TestOnlineScheduleActivity.java
@@ -0,0 +1,16 @@
+package com.example.mad12.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mad12.R;
+
+public class TestOnlineScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_online_schedule);
+    }
+}
